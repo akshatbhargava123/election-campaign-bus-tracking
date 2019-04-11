c3cea656daa65d201b62f6ab0e8b894be86e2dc2
akshat <akshatbhargavadeepa@gmail.com>
2019-04-11 16:37:07 +0530

    designed add driver form

--- pages/add-driver.vue
+++ pages/add-driver.vue
@@ -1,13 +1,77 @@
 <template>
-  <v-container fluid fill-height>
-    <v-layout justify-center align-center>
-      <h1>Add Driver</h1>
+  <v-container fluid>
+    <v-layout justify-center>
+      <v-form ref="form" v-model="valid" lazy-validation class="form">
+        <v-text-field
+          v-model="newDriver.name"
+          :rules="[v => !!v || 'Name is required']"
+          label="Name of the Driver is required"
+          required
+        ></v-text-field>
+        <v-text-field
+          v-model="newDriver.phone"
+          :rules="[v => (v && v.length <= 10) || 'Enter a valid mobile number']"
+          label="Driver's Mobile number will be used by them for logging in"
+          required
+        ></v-text-field>
+        <v-text-field
+          v-model="newDriver.password"
+          :rules="[v => !!v || 'Driver login password is required for driver to log into the app to start tracking']"
+          label="Driver Login Password"
+          required
+        ></v-text-field>
+
+        <v-btn
+          align-center
+          block
+          dark
+          color="indigo"
+          class="form-button"
+          @click="addDriver"
+        >
+          Add Driver
+        </v-btn>
+        <v-btn
+          align-center
+          block
+          dark
+          color="info"
+          class="form-button"
+          @click="resetForm"
+        >
+          Reset Form
+        </v-btn>
+      </v-form>
     </v-layout>
   </v-container>
 </template>
 
 <script>
 export default {
-  name: 'AddDriver'
+  name: 'AddDriver',
+  data: () => ({
+    valid: true,
+    newDriver: {
+      name: '',
+      email: '',
+      password: ''
+    }
+  }),
+  methods: {
+    addDriver() {
+      console.log(this.newDriver);
+      this.$refs.form.reset();
+    },
+    resetForm() {
+      this.newDriver = {};
+      this.$refs.form.reset();
+    }
+  }
 };
 </script>
+
+<style scoped>
+.form {
+  width: 60%;
+}
+</style>
